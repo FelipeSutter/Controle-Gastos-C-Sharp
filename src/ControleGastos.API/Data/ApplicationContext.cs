20ddd99a466caa291a030390d6497a2ff5d68bc9
FelipeSutter <lipeps61@gmail.com>
2024-03-15 15:05:24 -0300

    feat: Criado entidade Apagar, com model, map, e migrations

--- src/ControleGastos.API/Data/ApplicationContext.cs
+++ src/ControleGastos.API/Data/ApplicationContext.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ControleGastos.API.Data.Mappings;
 using ControleGastos.API.Domain.Models;
 using Microsoft.EntityFrameworkCore;
@@ -15,11 +11,15 @@ namespace ControleGastos.API.Data
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<NaturezaDeLancamento> Naturezas { get; set; }
 
+        public DbSet<Apagar> Apagar { get; set; }
+
+
         // Isso cria o modelo definido dentro do UsuarioMap
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UsuarioMap());
             modelBuilder.ApplyConfiguration(new NaturezaDeLancamentoMap());
+            modelBuilder.ApplyConfiguration(new ApagarMap());
         }
 
     }
---
+++ src/ControleGastos.API/Domain/Models/Apagar.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleGastos.API.Domain.Models {
+    public class Apagar {
+
+        [Key]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "O campo de Descrição é obrigatório.")]
+        public string Descricao { get; set; } = string.Empty;
+
+        public string? Observacao { get; set; } = string.Empty;
+
+        [Required]
+        public long IdUsuario { get; set; }
+
+        public Usuario Usuario { get; set; } // de quem é o titulo a pagar
+
+        [Required]
+        public long IdNatureza { get; set; }
+
+        public NaturezaDeLancamento Natureza { get; set; } // qual a natureza desse titulo a pagar
+
+        [Required(ErrorMessage = "O campo de ValorOriginal é obrigatório.")]
+        public double ValorOriginal { get; set; }
+
+        [Required(ErrorMessage = "O campo de ValorPago é obrigatório.")]
+        public double ValorPago { get; set; }
+
+        [Required]
+        public DateTime DataCadastro { get; set; }
+
+        [Required(ErrorMessage = "O campo de DataVencimento é obrigatório.")]
+        public DateTime DataVencimento { get; set; }
+
+        public DateTime? DataInativacao { get; set; }
+
+        public DateTime? DataReferencia { get; set; }
+
+        public DateTime? DataPagamento { get; set; }
+    }
+}
